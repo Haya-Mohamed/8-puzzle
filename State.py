5cf99125dfda846e055b9359c4348f082313d0b5
Youssef-Darwish <yousef952009@hotmail.com>
2018-10-22 00:04:56 +0200

    Draft code for BFS method for 8-puzzle

--- State.py
+++ State.py
@@ -24,22 +24,27 @@ class State():
         self.blank_position = blank
         self.action = action
         self.children = [] 
-    
+        # self.get_children()
                  
     def print(self):
          for i in range(3):
-            for j in range(3):
-                print(self.board[i][j] + ' ')
-            print('\n')
-            
-            
+            print('{} {} {}'.format(self.board[i][0],self.board[i][1],self.board[i][2]))
+         print('\n')
+
+
+    def print_children(self):
+        for child in self.children:
+            child.print()       
+
+    def __eq__(self, other): 
+        return self.board == other.board
+        
             
     def get_children(self):
         left = (self.blank_position[0], self.blank_position[1] - 1)
         right = (self.blank_position[0], self.blank_position[1] + 1)
         up = (self.blank_position[0] - 1, self.blank_position[1])
         down = (self.blank_position[0] + 1, self.blank_position[1])
-        
         if left[1] in range(3):
             self.children .append (self.move(Actions.LEFT, left))
         if right[1] in range(3):
@@ -51,11 +56,10 @@ class State():
         
         
     def move(self, action, new_blank_position):
-        new_board = [[self.board[x][y] for y in range(3)] for x in range(3)] 
+        new_board = [[self.board[x][y] for y in range(3)] for x in range(3)]
         new_board[new_blank_position[0]][new_blank_position[1]] = '0'
         new_board [self.blank_position[0]][self.blank_position[1]] = self.board [new_blank_position[0]][new_blank_position[1]] 
         child = State(new_board, self.cost + 1, action, new_blank_position)
-        #child.print()
         return child
         
         
--- eight_puzzle.py
+++ eight_puzzle.py
@@ -6,22 +6,27 @@ Created on Sat Oct 20 14:06:34 2018
 """
 import State as st
 import Util as ut
+import SearchAgents as agent
      
 def main():
+    
+    
+    
     str = '102345678'
     utility_obj = ut.Util()
     
     board, blank = utility_obj.get_board(str)
     obj = st.State(board, 0, None, blank)
-    obj.print()
-    obj.get_children()
+    #obj.print()
+    # obj.get_children()
     """
     for i in obj.children :
         i.print()
     """
     
-    goal_state = [['0','1','2'],['3','4','5'],['6','7','8']]
-    print (obj.test_goal(goal_state))
+  #  print (obj.test_goal(goal_state))
+    
+    bfs = agent.Search_Agents.BFS(agent.Search_Agents.BFS,obj)
     
 if __name__ == '__main__':
     main()
